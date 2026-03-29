7fecfc6f36a86ce377329b7b5d683e4fc3058ae6
Sawera Ghaffar <saweraghaffar16@gmail.com>
2026-03-29 13:24:20 +0800

    Update

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Students s1 = new Students();
+        s1.name = "Sawera Ghaffar";
+        s1.rollNumber = 5048;
+        s1.displayInfo();
+
+        Students s2 = new Students();
+        s2.name = "Laiba sajid";
+        s2.rollNumber = 5049;
+        s2.displayInfo();
+
+    }
+}
